8cea376aa28f59da72f98f20a00788732e3c3e9f
vidu1999 <vidurakavinda1999@gmail.com>
2025-03-03 14:45:42 +0530

    Fix .glm

--- src/app/compenents/models/Robot.jsx
+++ src/app/compenents/models/Robot.jsx
@@ -4,7 +4,7 @@ import { useGLTF } from '@react-three/drei'
 
 export default function Model(props) {
   try {
-  const { nodes, materials, animations } = useGLTF("/models/low_poly_stone_arche-transformed.glb");
+  const { nodes, materials, animations } = useGLTF("/public/models/low_poly_stone_arche-transformed.glb");
 
   return (
     <group {...props} dispose={null} position={[-0.2,-2,0]} scale={[7,7,7]} rotation={[0,1.6,0]}>
