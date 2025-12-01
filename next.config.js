36d366496a7cf70e4922a29f8c16b38eabaf2e52
vidu1999 <vidurakavinda1999@gmail.com>
2025-12-01 08:26:35 +0530

    Remove assetPrefix to fix 404 errors

--- next.config.js
+++ next.config.js
@@ -2,14 +2,13 @@
 const isProd = process.env.NODE_ENV === 'production';
 const repo = 'portfolio';
 const basePath = isProd ? `/${repo}` : '';
-const assetPrefix = isProd ? `/${repo}/` : undefined; // ensure trailing slash in production
+
 
 const nextConfig = {
   // Static export for GitHub Pages
   output: 'export',
   trailingSlash: true,
   basePath,
-  assetPrefix,
   images: {
     unoptimized: true,
   },
