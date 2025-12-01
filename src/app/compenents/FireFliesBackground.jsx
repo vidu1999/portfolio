5068b5f417153d4b323cfdf9d823aea3ce2d6024
vidu1999 <vidurakavinda1999@gmail.com>
2025-12-01 08:56:59 +0530

    img path adjust

--- src/app/compenents/FireFliesBackground.jsx
+++ src/app/compenents/FireFliesBackground.jsx
@@ -5,56 +5,56 @@ import Image from 'next/image';
 const getRandomImage = () => {
     const randomIndex = Math.floor(Math.random() * ImgList.length); // Generate a random index
     return ImgList[randomIndex].name; // Return the image object
-  };
-  
+};
+
 
 const createFirefly = () => (
-    
+
     {
-id: Math.random(),
-top: `${Math.random()*100}%`,
-left: `${Math.random()*100}%`,
-animationDuration: `${Math.random()*5 + 5}s`,
-img:`${getRandomImage()}`
-})
+        id: Math.random(),
+        top: `${Math.random() * 100}%`,
+        left: `${Math.random() * 100}%`,
+        animationDuration: `${Math.random() * 5 + 5}s`,
+        img: `${getRandomImage()}`
+    })
 
-const FireFliesBackground = () =>{
+const FireFliesBackground = () => {
 
-const [fireflies, setFireflies] = useState([])
-//const randomImage = getRandomImage();
-useEffect(() => {
-    
-    const addFireflyPeriodically = () => {
-    const newFirefly = createFirefly();
-    setFireflies(currentFireflies =>
-[
-        ... currentFireflies.slice(-14),
-        newFirefly]);
-    }
-    const interval = setInterval(addFireflyPeriodically, 1000);
+    const [fireflies, setFireflies] = useState([])
+    //const randomImage = getRandomImage();
+    useEffect(() => {
 
-return () => clearInterval(interval)
+        const addFireflyPeriodically = () => {
+            const newFirefly = createFirefly();
+            setFireflies(currentFireflies =>
+                [
+                    ...currentFireflies.slice(-14),
+                    newFirefly]);
+        }
+        const interval = setInterval(addFireflyPeriodically, 1000);
 
- }, [])
- return (
-    <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
-    {fireflies.map((firefly) => {
-        console.log(fireflies)
+        return () => clearInterval(interval)
+
+    }, [])
     return (
-    <div
-    key={firefly.id}
-    className="absolute rounded-full w-[20px] h-[20px] bg-firefly-radial"
-    style={{
-    top: firefly.top,
-    left: firefly.left,
-    animation: `move ${firefly.animationDuration} infinite alternate`
-    }}
-    ><Image src={`/icon/${firefly.img}`} alt={`Random Image ${firefly.img}` }  width={300} // Set width as needed
-    height={200}// Set width as needed
-    /></div>
-);
-})}
-    </div>
+        <div className="fixed top-0 left-0 w-full h-full -z-10 overflow-hidden">
+            {fireflies.map((firefly) => {
+                console.log(fireflies)
+                return (
+                    <div
+                        key={firefly.id}
+                        className="absolute rounded-full w-[20px] h-[20px] bg-firefly-radial"
+                        style={{
+                            top: firefly.top,
+                            left: firefly.left,
+                            animation: `move ${firefly.animationDuration} infinite alternate`
+                        }}
+                    ><Image src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/icon/${firefly.img}`} alt={`Random Image ${firefly.img}`} width={300} // Set width as needed
+                        height={200}// Set width as needed
+                        /></div>
+                );
+            })}
+        </div>
     );
 }
 
